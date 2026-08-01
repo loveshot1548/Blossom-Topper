96727e5f36a69c13b588131f5ecccdf7041b768a
loveshot1548 <loveshot1548@gmail.com>
2026-08-01 20:28:48 +0900

    fix: Tailwind 설정 파일 추가 및 CSS 수동 연결

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import App from "./App"; // 이렇게 수정하면 .js나 .jsx 둘 다 찾아냅니다!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
